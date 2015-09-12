a1a0d91d20722ad716eb2e53f667bb884dfb49b7
woliveiras <w.oliveira542@gmail.com>
2015-09-12 17:56:41 -0300

    Site updated: 2015-09-12 17:56:33

--- js/script.js
+++ js/script.js
@@ -41,7 +41,8 @@
       url = $this.attr('data-url'),
       encodedUrl = encodeURIComponent(url),
       id = 'article-share-box-' + $this.attr('data-id'),
-      offset = $this.offset();
+      offset = $this.offset(),
+      articleName = $('.article-title').text();
 
     if ($('#' + id).length){
       var box = $('#' + id);
@@ -55,9 +56,8 @@
         '<div id="' + id + '" class="article-share-box">',
           '<input class="article-share-input" value="' + url + '">',
           '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + "/&text=Acabei de ler:" + articleName + "&via=@woliveiras" + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
             '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
             '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
           '</div>',
         '</div>'
