4a0825979a0fc8b5ca157a50030b267f8529e171
woliveiras <w.oliveira542@gmail.com>
2020-10-03 12:54:25 -0300

    refac(pages): update font-size for post-list

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -8,7 +8,20 @@ const appendSuffix = n => {
 module.exports = function dateFilter(value) {
   const dateObject = new Date(value);
 
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const months = [
+    'Janeiro', 
+    'Fevereiro', 
+    'Março', 
+    'Abril', 
+    'Maio', 
+    'Junho', 
+    'Julho', 
+    'Agosto', 
+    'Setembro', 
+    'Outubro', 
+    'Novembro', 
+    'Dezembro'
+  ];
   const dayWithSuffix = appendSuffix(dateObject.getDate());
 
   return `${dayWithSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
