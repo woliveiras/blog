e451ffd94644817d463f0dc44e451e677c887603
William Oliveira <w.oliveira542@gmail.com>
2024-03-16 11:40:36 +0100

    feat: update footer

--- src/navigation.js
+++ src/navigation.js
@@ -127,52 +127,23 @@ export const headerData = {
 };
 
 export const footerData = {
-  // links: [
-  //   {
-  //     title: 'Product',
-  //     links: [
-  //       { text: 'Features', href: '#' },
-  //       { text: 'Security', href: '#' },
-  //       { text: 'Team', href: '#' },
-  //       { text: 'Enterprise', href: '#' },
-  //       { text: 'Customer stories', href: '#' },
-  //       { text: 'Pricing', href: '#' },
-  //       { text: 'Resources', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Platform',
-  //     links: [
-  //       { text: 'Developer API', href: '#' },
-  //       { text: 'Partners', href: '#' },
-  //       { text: 'Atom', href: '#' },
-  //       { text: 'Electron', href: '#' },
-  //       { text: 'AstroWind Desktop', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Support',
-  //     links: [
-  //       { text: 'Docs', href: '#' },
-  //       { text: 'Community Forum', href: '#' },
-  //       { text: 'Professional Services', href: '#' },
-  //       { text: 'Skills', href: '#' },
-  //       { text: 'Status', href: '#' },
-  //     ],
-  //   },
-  //   {
-  //     title: 'Company',
-  //     links: [
-  //       { text: 'About', href: '#' },
-  //       { text: 'Blog', href: '#' },
-  //       { text: 'Careers', href: '#' },
-  //       { text: 'Press', href: '#' },
-  //       { text: 'Inclusion', href: '#' },
-  //       { text: 'Social Impact', href: '#' },
-  //       { text: 'Shop', href: '#' },
-  //     ],
-  //   },
-  // ],
+  links: [
+    {
+      title: 'Iniciativas que participo',
+      links: [
+        { text: 'Núcleo de Tecnologia do MTST', href: 'https://nucleodetecnologia.com.br/' },
+        { text: 'Laboratória', href: 'https://www.laboratoria.la/' },
+      ],
+    },
+    {
+      title: 'Iniciativas das quais já fiz parte',
+      links: [
+        { text: 'perifaCode', href: 'https://perifacode.com/' },
+        { text: 'Tecnogueto', href: 'https://www.linkedin.com/company/tecnogueto/' },
+        { text: 'Training Center', href: 'https://github.com/training-center' },
+      ],
+    },
+  ],
   // secondaryLinks: [
   //   { text: 'Terms', href: getPermalink('/terms') },
   //   { text: 'Privacy Policy', href: getPermalink('/privacy') },
